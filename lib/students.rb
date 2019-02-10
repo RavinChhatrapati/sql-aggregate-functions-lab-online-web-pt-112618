7e2dfbfb7e97b9e482b90103fcb74512684b1612
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-02-10 16:33:48 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -17,6 +17,6 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-"SELECT AVG(gpa) FROM students WHERE id > 6 ;"
-"SELECT AVG(x) FROM students;"
+"SELECT AVG(gpa) FROM students WHERE id > 6;"
+
 end
